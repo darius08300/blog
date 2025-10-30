75e5cbd3295e29d00c1b50b480c8751ec4d05da1
Darius Andronesi <164866942+darius08300@users.noreply.github.com>
2025-10-30 17:58:43 +0200

    remove julie creative portfolio

--- contact_form.php
+++ contact_form.php
@@ -74,7 +74,7 @@ if(isset($_POST['name'])) {
 <html lang="en">
 
 <head>
-  <title>Julie Creative Portfolio</title>
+  <title>Darius Blog</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
